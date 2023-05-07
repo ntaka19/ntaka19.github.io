2d9fe13cd1ae632f008cef0abdaead0c4f6297b6
Naruaki Takano <naruakitakano@gmail.com>
2023-05-08 07:27:20 +0900

    dashboard

---
+++ files/src/dashboard/dashboard.rst
@@ -0,0 +1,6 @@
+Weather data by Open-Meteo.com<https://open-meteo.com/>
+
+.. figure:: ./forecast.png 
+   :align: center
+
+
--- files/src/dashboard/forecast.py
+++ files/src/dashboard/forecast.py
@@ -1,32 +1,28 @@
-Weather data by Open-Meteo.com<https://open-meteo.com/>
+import requests
+import json
+import matplotlib.pyplot as plt
 
-.. jupyter-execute::
-  :hide-code:
-  
-  import requests
-  import json
-  import matplotlib.pyplot as plt
+url = 'https://api.open-meteo.com/v1/forecast?latitude=35.69&longitude=139.69&hourly=temperature_2m,rain,showers&timezone=Asia%2FTokyo'
 
-  url = 'https://api.open-meteo.com/v1/forecast?latitude=35.69&longitude=139.69&hourly=temperature_2m,rain,showers&timezone=Asia%2FTokyo'
+response = requests.get(url)
+data = json.loads(response.text)
 
-  response = requests.get(url)
-  data = json.loads(response.text)
+#plt.rcParams['figure.figsize'] = [6, 3]
+#plt.rcParams['figure.facecolor'] = "black"
 
-  #plt.rcParams['figure.figsize'] = [6, 3]
-  #plt.rcParams['figure.facecolor'] = "black"
-
-  def draw_chart(data):
+def draw_chart(data):
     labels = data['hourly']['time']
-    
+
     new_labels = [ '-'.join(element.split("-")[1:]) for element in labels]
-    #new_labels = [ element.split("T")[0]  for element in labels]
+#new_labels = [ element.split("T")[0]  for element in labels]
     rain = data['hourly']['rain']
     showers = data['hourly']['showers']
     temperature = data['hourly']['temperature_2m']
 
     fig, ax1 = plt.subplots(figsize=(10, 4))
     plt.xticks(range(0, len(new_labels), 12), rotation=90)
-    
+
+
     ax1.plot(new_labels, temperature, label='Temperature', color='red')
     ax2 = ax1.twinx()
 
@@ -38,12 +34,12 @@ Weather data by Open-Meteo.com<https://open-meteo.com/>
     plt.title('Weather Forecast(TOKYO) ' + labels[0].split('-')[0] )
     ax1.legend(loc='upper left')
     ax2.legend(loc='upper right')
-    plt.show()
-
     
-    fig.tight_layout()
+    plt.tight_layout()
+    plt.savefig('forecast.png') 
 
-  draw_chart(data)
+
+draw_chart(data)
 
 
 
