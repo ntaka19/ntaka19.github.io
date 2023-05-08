48001c6c539ee8d19e790893d9689088ea11bc31
Naruaki Takano <naruakitakano@gmail.com>
2023-05-09 08:24:29 +0900

    test

--- files/src/dashboard/forecast.py
+++ files/src/dashboard/forecast.py
@@ -81,7 +81,7 @@ def draw_chart(data):
 
     plt.tight_layout()
     #plt.savefig('forecast.png')
-    plt.savefig('../../figures/forecast.png')
+    plt.savefig('figures/forecast.png')
 
 draw_chart(data)
 
