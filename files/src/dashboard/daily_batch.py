714a42fd4b0dfd4f20eae07b64829db28bea17ff
ntaka19 <naruakitakano@gmail.com>
2025-03-04 23:02:48 +0900

    debug

--- files/src/dashboard/daily_batch.py
+++ files/src/dashboard/daily_batch.py
@@ -221,7 +221,7 @@ class D002_FX_Daily:
     def market_summary_html(self):
         #chatgpt = ChatGPTWrapper()
         perplexity = PerplexityWrapper()
-        prompt = "本日時点の世界経済のニュースを述べよ。顕著な株価の増減を述べよ。また、USD/JPYの分析をせよ。最後に今後の重要なイベントを述べよ。" #:  {first}".format(first=json.dumps(self.data_json))                                
+        prompt = "本日時点の世界経済のニュースを述べよ。株価の増減を述べよ。また、USD/JPYの分析をせよ。最後に今後の重要なイベントを述べよ。" #:  {first}".format(first=json.dumps(self.data_json))                                
         market_summary_text = perplexity.GetResponse(prompt)
         print(market_summary_text)
 
